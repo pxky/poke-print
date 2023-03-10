c2f842ae357f7359ec7f9044e480a6a91b9fec7d
poky <121595762+pxky@users.noreply.github.com>
2023-03-10 13:08:34 +1300

    Add files via upload

--- pprint.js
+++ pprint.js
@@ -1,13 +1,22 @@
-//this code is awful please dont ever use it
+// this code is awful please dont ever use it
 const chalk = require('chalk')
 const chalkAnimation = require('chalk-animation')
 const gradientString = require('gradient-string')
 
 const pprint = (message, properties) => {
+    // return if no properties array
+    if (!Array.isArray(properties)) {
+        console.log(`${message}`)
+        return
+    }
+
+    // evaluate properties
     let propertiesString = ''
     for (let i = 0; i < properties.length; i++) {
         propertiesString = propertiesString.concat(`['${properties[i]}']`)
     }
+
+    // find and log with correct module and properties
     if (eval('chalk'.concat(propertiesString))) {
         console.log(eval('chalk'.concat(propertiesString))(`${message}`))
     }
@@ -17,11 +26,11 @@ const pprint = (message, properties) => {
     else if (eval('chalkAnimation'.concat(propertiesString))) {
         eval('chalkAnimation'.concat(propertiesString))(`${message}`)
     }
+
+    // invalid properties
     else {
-        console.log(`error w/ properties: ${message}`)
+        console.log(`invalid properties: ${message}`)
     }
 }
 
-module.exports = {
-    pprint
-}
+module.exports = pprint
